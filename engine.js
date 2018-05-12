88ed1432480caf92d00b0d73c6f7a7e8481b9094
Lxxyx <Lxxyxzj@gmail.com>
2018-05-13 01:14:14 +0800

    fix:

--- engine.js
+++ engine.js
@@ -5,8 +5,8 @@ var map = require('lodash.map');
 var longest = require('longest');
 var rightPad = require('right-pad');
 
-var filter = function(array) {
-  return array.filter(function(x) {
+var filter = function (array) {
+  return array.filter(function (x) {
     return x;
   });
 };
@@ -17,17 +17,12 @@ var filter = function(array) {
 module.exports = function (options) {
 
   var types = options.types;
-  var format = options.format || '[emoji] [name]: [subject]';
 
-  var length = longest(Object.keys(types)).length + 2;
+  var length = longest(Object.keys(types)).length + 1;
   var choices = map(types, function (type, key) {
-    var name = type.name || key;
     return {
-      name: type.emoji + '  ' + rightPad(name + ':', length) + ' ' + type.description,
-      value: {
-        emoji: type.emoji,
-        name: name,
-      }
+      name: rightPad(key + ':', length) + ' ' + type.description,
+      value: key
     };
   });
 
@@ -43,7 +38,9 @@ module.exports = function (options) {
     //
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
-    prompter: function(cz, commit) {
+    prompter: function (cz, commit) {
+      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+
       // Let's ask some questions of the user
       // so that we can populate our commit
       // template.
@@ -59,41 +56,73 @@ module.exports = function (options) {
           choices: choices
         }, {
           type: 'input',
+          name: 'scope',
+          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n'
+        }, {
+          type: 'input',
           name: 'subject',
           message: 'Write a short, imperative tense description of the change:\n'
         }, {
           type: 'input',
           name: 'body',
-          message: 'Provide a longer description of the change:\n'
+          message: 'Provide a longer description of the change: (press enter to skip)\n'
+        }, {
+          type: 'confirm',
+          name: 'isBreaking',
+          message: 'Are there any breaking changes?',
+          default: false
+        }, {
+          type: 'input',
+          name: 'breaking',
+          message: 'Describe the breaking changes:\n',
+          when: function (answers) {
+            return answers.isBreaking;
+          }
+        }, {
+          type: 'confirm',
+          name: 'isIssueAffected',
+          message: 'Does this change affect any open issues?',
+          default: false
         }, {
           type: 'input',
           name: 'issues',
-          message: 'List any issues closed by this change:\n'
+          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
+          when: function (answers) {
+            return answers.isIssueAffected;
+          }
         }
-      ]).then(function(answers) {
+      ]).then(function (answers) {
 
         var maxLineWidth = 100;
 
         var wrapOptions = {
           trim: true,
           newline: '\n',
-          indent:'',
+          indent: '',
           width: maxLineWidth
         };
 
+        console.log(answers);
+
+
+        // parentheses are only needed when a scope is present
+        var scope = answers.scope.trim();
+        scope = scope ? '(' + answers.scope.trim() + ')' : '';
+
         // Hard limit this line
-        var head = format.replace(/\[emoji\]/g, answers.type.emoji)
-          .replace(/\[name\]/g, answers.type.name)
-          .replace(/\[subject\]/g, answers.subject.trim())
-          .slice(0, maxLineWidth);
-        // var head = (answers.type + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
 
         // Wrap these lines at 100 characters
         var body = wrap(answers.body, wrapOptions);
 
-        var issues = wrap(answers.issues, wrapOptions);
+        // Apply breaking change prefix, removing it if already present
+        var breaking = answers.breaking ? answers.breaking.trim() : '';
+        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
+        breaking = wrap(breaking, wrapOptions);
 
-        var footer = filter([ issues ]).join('\n\n');
+        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
+
+        var footer = filter([breaking, issues]).join('\n\n');
 
         commit(head + '\n\n' + body + '\n\n' + footer);
       });
